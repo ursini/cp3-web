c714828fbbb3e4068d5b3dc2b669d054f7ecc779
ursini <leonardo.ursini2008@gmail.com>
2026-05-20 09:31:16 -0300

    chore: Adicionando toda a estrutura

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+
+
+let vinhos =[
+    {nome: 'casillero del diablo ', tipo: 'Tinto', safra: '2018', estoque: '20' }
+    {nome: 'Casal Garcia', tipo: 'Branco', safra: '2000', estoque:'14'}
+    {nome: 'Casal Mendes', tipo: 'Rosé', safra: '1982', estoque:'8'}
+    {nome: 'Aurora Rosé', tipo: 'Rosé', safra: '1892', estoque:'1'}
+]
+
+function adicionarVinho()
